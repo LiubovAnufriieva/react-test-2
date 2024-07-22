673ae8fa2ac20203df8165d50a6c85e60967b138
Lyuzika <lyuzika@gmail.com>
2024-07-22 15:44:21 +0300

    react icons add

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,4 +1,6 @@
 import css from "./FriendList.module.css"
+import { FcOnlineSupport } from "react-icons/fc"
+import { IoCloudOffline } from "react-icons/io5"
 
 export default function FriendListItem( {friend: { avatar, name, isOnline }}) {
   return (
@@ -6,7 +8,7 @@ export default function FriendListItem( {friend: { avatar, name, isOnline }}) {
       <img className={css.ava} src={avatar} alt="Avatar" width="48" />
       <p className={css.name}>{name}</p>
       <p className={isOnline ? css.online : css.offline}>
-        {isOnline ? 'Online' : 'Offline'}</p>
+        {isOnline ? <FcOnlineSupport /> : <IoCloudOffline /> }</p>
     </div>
   )
 }
